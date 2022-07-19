7498e5f412847435db4344b3f054eb37abd9d580
rurika21 <shironyanmaru@gmail.com>
2022-07-19 12:52:25 +0900

    [add]記事一覧画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index(Post $post)
+    {
+        return view('posts/index')->with(['posts' => $post->get()]);  
+    }
 }
+?>
