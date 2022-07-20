82667ad51c9bd49374de378f5fd5dc7e475ebcc0
rurika21 <shironyanmaru@gmail.com>
2022-07-20 17:38:44 +0900

    [add]記事詳細画面

--- app/Post.php
+++ app/Post.php
@@ -8,6 +8,11 @@ class Post extends Model
 {
     public function getPaginateByLimit(int $limit_count = 10)
     {
-        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+        return $this->orderBy('updated_at', 'DESC')->getpaginate($limit_count);
     }
+    
+    protected $fillable = [
+    'title',
+    'body',
+];
 }
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,5 @@
 
 Route::get('/', 'PostController@index');
 Route::get('/posts/{post}', 'PostController@show');
+Route::get('/posts/create', 'PostController@create');
+Route::post('/posts', 'PostController@store');
