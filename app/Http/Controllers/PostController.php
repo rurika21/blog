e101c75dabcadf7ef0281e6cb8401a35a495df83
rurika21 <shironyanmaru@gmail.com>
2022-07-20 17:03:32 +0900

    記事詳細画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest; // useする
 
 class PostController extends Controller
 {
@@ -11,21 +11,21 @@ class PostController extends Controller
     {
         return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
     }
-    
+
     public function show(Post $post)
     {
         return view('posts/show')->with(['post' => $post]);
     }
-    
+
     public function create()
     {
-    return view('posts/create');
+        return view('posts/create');
     }
-    
-    public function store(Request $request, Post $post)
+
+    public function store(Post $post, PostRequest $request) // 引数をRequest->PostRequestにする
     {
-    $input = $request['post'];
-    $post->fill($input)->save();
-    return redirect('/posts/' . $post->id);
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
